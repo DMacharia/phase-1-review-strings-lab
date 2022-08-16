76eec45cd9b0d9fb9a0859312fa728399259a681
Daniel Macharia <danielhuios@gmail.com>
2022-08-16 07:06:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+let currentUser = "Wafula Chebukati";
+
+// let welcomeMessage = "Welcome to Flatbook, ";
+
+// let welcomeMessage = "Welcome to Flatbook, " + currentUser;
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
